eb762b11a6bb327d178a91b8b79efd419432d7f7
Danielle Blom <dlblom91@gmail.com>
2020-10-31 15:59:56 -0400

    Install cors and enable express json middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3001;
+
+app.use(express.json());
+app.use(cors());
 
 const db = require("../database/index");
 
