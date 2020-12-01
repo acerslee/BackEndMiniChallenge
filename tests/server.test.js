b0580260ea16f1dda2d95fdcc1ebcd5ed75ba7fe
Danielle Blom <dlblom91@gmail.com>
2020-12-01 11:38:36 -0600

    Complete tests for GET by id and PUT by id

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,7 +4,6 @@ const db = require("../database");
 const app = require("../server/app");
 const supertest = require("supertest");
 
-// set up & teardown
 beforeAll(async () => {
   await db.connection.query(`CREATE TABLE IF NOT EXISTS pets (
   id integer AUTO_INCREMENT UNIQUE PRIMARY KEY,
@@ -14,10 +13,12 @@ beforeAll(async () => {
 )`);
 });
 
-// set up & tear down for tests
 beforeEach(async () => {
   await db.connection.query(
     `INSERT INTO pets (name, type, age) VALUES ("Grumpy Cat", "Cat", 10)`
+  );
+  await db.connection.query(
+    `INSERT INTO pets (name, type, age) VALUES ("Bark Twain", "Dog", 7)`
   );
 });
 
@@ -40,52 +41,78 @@ describe("GET /api/pets ", () => {
         type: "Cat",
         age: 10,
       },
+      {
+        id: 2,
+        name: "Bark Twain",
+        type: "Dog",
+        age: 7,
+      },
     ]);
-    expect(response.body.length).toEqual(1);
+    expect(response.body.length).toEqual(2);
     expect(response.statusCode).toBe(200);
   });
+});
 
-  // describe("GET /api/pets/:id ", () => {
-  //   test("It should respond with a single pet by id", async () => {
-  //     const response = await supertest(app).get("/api/pets/1");
-  //     expect(response.body).toEqual([
-  //       {
-  //         id: 1,
-  //         name: "Oliver",
-  //         type: "Dog",
-  //         age: 1,
-  //       },
-  //     ]);
-  //     expect(response.body.length).toEqual(1);
-  //     expect(response.statusCode).toBe(200);
-  //   });
-  // });
+describe("GET /api/pets/:id ", () => {
+  test("It should respond with a single pet by id", async () => {
+    const newPet = await supertest(app).post("/api/pets").send({
+      name: "Bob Barker",
+      type: "Dog",
+      age: 4,
+    });
+    await supertest(app)
+      .get("/api/pets/" + 3)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.name).toBe(newPet.name);
+        expect(response.body.length).toBe(1);
+      });
+  });
+});
 
-  // describe("POST /api/pets", () => {
-  //   test("It should add a new pet to the database", async () => {
-  //     const newPet = await supertest(app).post("/api/pets").send({
-  //       name: "Snoopy",
-  //       type: "Dog",
-  //       age: 5,
-  //     });
+describe("POST /api/pets", () => {
+  test("It should add a new pet to the database", async () => {
+    const newPet = await supertest(app).post("/api/pets").send({
+      name: "Mary Puppins",
+      type: "Dog",
+      age: 5,
+    });
 
-  //     expect(newPet.statusCode).toBe(200);
+    await supertest(app)
+      .get("/api/pets")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.length).toBe(3);
+      });
+  });
+});
 
-  //     const response = await supertest(app).get("/api/pets");
-  //     expect(response.body.length).toBe(16);
-  //   });
-  // });
+describe("PUT /api/pets/:id", () => {
+  test("It should update a pet by id", async () => {
+    const newPet = await supertest(app).post("/api/pets").send({
+      name: "Cindy Pawford",
+      type: "Kitten",
+      age: 1,
+    });
 
-  // describe("PUT /api/pets/:id", () => {
-  //   test("It should update a pet by id", async () => {
-  //     const updatedPet = await supertest(app).put("/api/pets/1").send({
-  //       name: "Bruce",
-  //       type: "Dog",
-  //       age: 2,
-  //     });
-  //   });
-  //   expect(updatedPet.statusCode).toBe(200);
-  //   const response = supertest(app).get("/api/pets");
-  //   expect(response.body.length).toBe(16);
-  // });
+    const updateNewPet = {
+      name: "Cindy Clawford",
+      type: "Cat",
+      age: 2,
+    };
+
+    await supertest(app)
+      .put("/api/pets/" + 3)
+      .send(updateNewPet)
+      .expect(200)
+      .then(async (response) => {
+        const updatedPet = await db.connection.query(
+          `SELECT * FROM pets WHERE id = 3`
+        );
+        expect(updatedPet).toBeTruthy();
+        expect(updatedPet.name).toBe(updatedPet.name);
+        expect(updatedPet.type).toBe(updatedPet.type);
+        expect(updatedPet.age).toBe(updatedPet.age);
+      });
+  });
 });
