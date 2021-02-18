52aa8920a4d864dc9085df6e5e2ebcc1024d5e26
Alex Lee <ahjlee712@gmail.com>
2021-02-18 00:05:40 -0500

    Added the query and server calls

--- database/index.js
+++ database/index.js
@@ -13,25 +13,78 @@ var connection = mysql.createConnection({
 connection.connect();
 
 // TODO: write a query to get all data from pets table
-const getPetData = () => {};
+const getPetData = (callback) => {
+  connection.query('SELECT * FROM pets', (err, data) => {
+    if (err) {
+      console.log(err, 'not getting any pets');
+    } else {
+      callback(data);
+    }
+  })
+};
 
 // TODO: write a query to get a pet by id
-const getPetById = () => {};
+const getPetById = (petParams, callback) => {
+  connection.query ('SELECT * FROM pets WHERE id = ?', [petParams.id] , (err, data) => {
+  if (err){
+    callback(err)
+  } else {
+    callback(data)
+    }
+  })
+};
 
 // TODO: write a query to create a new pet
-const addPet = () => {};
+const addPet = (petData, callback) => {
+  connection.query('INSERT INTO pets (name, type, age) VALUES (?, ?, ?)', [petData.name, petData.type, petData.age], (err,data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(data)
+    }
+  })
+};
 
 // TODO: write a query to update a pet's name, type and age based on an id
-const updatePetById = () => {};
+const updatePetById = (petParams, petData, callback) => {
+  connection.query('UPDATE pets SET name = ?, type = ?, age = ? WHERE id = ?', [petParams.id, petData.name, petData.type, petData.age], (err,data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(data);
+    }
+  })
+};
 
 // TODO: write a query to update only the pet's age by id
-const updatePetAgeById = () => {};
+const updatePetAgeById = (petParams, petData, callback) => {
+  connection.query('UPDATE pets SET age = ? WHERE id = ?', [petParams.id, petData.age], (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(data);
+    }
+  })
+};
 
 // TODO: write a query to delete a pet by id
-const deletePetById = () => {};
+const deletePetById = (petParams, callback) => {
+  connection.query('DELETE FROM pets WHERE id = ?', [petParams.id], (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(data);
+    }
+  })
+};
 
 // don't forget to export your methods!
 // NOTE: the connection is being exported for testing purposes only and has nothing to do with the functionality of the application
 module.exports = {
-  connection,
+  getPetData,
+  getPetById,
+  addPet,
+  updatePetById,
+  updatePetAgeById,
+  deletePetById
 };
--- server/app.js
+++ server/app.js
@@ -1,23 +1,77 @@
 const express = require("express");
-const app = express();
+const bodyparser = require('body-parser');
 const cors = require("cors");
 
-app.use(express.json());
+const app = express();
+
+app.use(express.json();
 app.use(cors());
 
 const db = require("../database/index");
 
 // TODO: create a server route to get all the pet data
-
+app.get('/api/pets', (req,res) => {
+  console.log('testing route');
+  db.getPetData((err, data) => {
+    if (err) {
+      throw(err);
+    } else {
+      res.status(200).send(data);
+    }
+  })
+});
 // TODO: create a server route to get a pet by id
+app.get('/api/pets/:id', (req, res) => {
+  db.getPetDataById(req.params, (err, data) => {
+    if (err) {
+      throw err
+    } else {
+      res.status(200).send(data);
+    }
+  })
+});
 
 // TODO: create a server route to create a new pet
-
+app.post('/api/pets', (req, res) => {
+  db.addPet(req.body, (err,data) => {
+    if (err) {
+      throw err
+    } else {
+      res.status(201).send(data);
+    }
+  })
+});
 // TODO: create a server route to update a pet's name, type and age based on an id
+app.put('/api/pets/:id', (req, res) => {
+  db.updatePetById(req.params, req.body, (err,data) => {
+    if (err) {
+      throw err
+    } else {
+      res.status(200).send(data):
+    }
+  })
+});
 
 // TODO: create a server route to update only a pet's age based on the id (Use PATCH)
+app.patch('/api/pets/:id', (req, res) => {
+  db.updatePetAgeById(req.params, req.body, (err, data) => {
+    if (err) {
+      throw err
+    } else {
+      res.status(200).send(data);
+    }
+  })
+});
 
 // TODO: create a server route to delete a pet by id
-
+app.delete('/api/pets', (req, res) => {
+  db.deletePetById(req.params, (err,data) => {
+    if (err) {
+      throw err
+    } else {
+      res.status(200).send(data);
+    }
+  })
+});
 // DO NOT ALTER THIS LINE
 module.exports = app;
